d94e7be023deba5a2861e6e2ba17624e70e3d752
Shartazsajidnahid <sajidnahid9658@gmail.com>
2022-02-16 19:26:15 +0600

    commented

--- java/src/main/java/workshop/FizzBuzz.java
+++ java/src/main/java/workshop/FizzBuzz.java
@@ -6,6 +6,7 @@ package workshop;
  * For factor of five print Buzz instead of the number
  * For numbers which are factors of both three and five print FizzBuzz instead of the number
  */
+//hey there
 public class FizzBuzz {
     public static String say(int number) {
         String strReturn = null;
