542516333f8cd777cc2e9f9bdeb62ee53b26fb87
Shartazsajidnahid <sajidnahid9658@gmail.com>
2022-02-17 11:53:11 +0600

    TriviaGame: ListOfQuestions: trying to implement a map instead of 4 linkedlists

--- java/src/main/java/workshop/triviagame/ListOfQuestions.java
+++ java/src/main/java/workshop/triviagame/ListOfQuestions.java
@@ -1,14 +1,29 @@
 package workshop.triviagame;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.*;
+
 
 public class ListOfQuestions{
-    LinkedList popQuestions = new LinkedList();
-    LinkedList scienceQuestions = new LinkedList();
-    LinkedList sportsQuestions = new LinkedList();
-    LinkedList rockQuestions = new LinkedList();
+
+    //TODO: implement a list-of-linkedlist or Map or something to replace 4 linkedlists
+
+    private HashMap<String,LinkedList<String>> question_list = new HashMap<>();
+    String[] category = new String[]{"Pop","Science","Sports","Rock"};
+
+    LinkedList<String> popQuestions = new LinkedList<String>();
+    LinkedList<String> scienceQuestions = new LinkedList<String>();
+    LinkedList<String> sportsQuestions = new LinkedList<String>();
+    LinkedList<String> rockQuestions = new LinkedList<String>();
 
     public ListOfQuestions(){
+        for (String type : category) {
+            question_list.put(type, new LinkedList<String>());
+        }
+
+
         for (int i = 0; i < 50; i++) {
             popQuestions.addLast("Pop Question " + i);
             scienceQuestions.addLast("Science Question " + i);
@@ -22,21 +37,15 @@ public class ListOfQuestions{
     }
 
     private String announce_question(int number) {
-        if (currentCategory(number) == "Pop")
-            return  (String) popQuestions.removeFirst();
-        else if (currentCategory(number) == "Science")
-            return (String) scienceQuestions.removeFirst();
-        else if (currentCategory(number) == "Sports")
-            return  (String) sportsQuestions.removeFirst();
-        else if (currentCategory(number) == "Rock")
-            return  (String) rockQuestions.removeFirst();
-
+        if (currentCategory(number) == "Pop")  return  (String) popQuestions.removeFirst();
+        else if (currentCategory(number) == "Science") return (String) scienceQuestions.removeFirst();
+        else if (currentCategory(number) == "Sports")  return  (String) sportsQuestions.removeFirst();
+        else if (currentCategory(number) == "Rock") return  (String) rockQuestions.removeFirst();
     }
 
 
     public String currentCategory(int number) {
-        String[] category_check = new String[]{"Pop","Science","Sports","Rock"};
-        return category_check[number % 4];
+        return category[number % 4];
     }
 
 }
